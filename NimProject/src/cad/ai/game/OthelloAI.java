f15c823aaaabaff13318793e91b7bdabd2c9c4ea
Nicholas Molina <nicholasemolina@gmail.com>
2019-02-20 13:53:34 -0500

    Move evaluation

--- NimProject/src/cad/ai/game/OthelloAI.java
+++ NimProject/src/cad/ai/game/OthelloAI.java
@@ -27,6 +27,13 @@ import java.util.ArrayList;
 public class OthelloAI extends AbstractAI {
     public OthelloGame game;  // The game that this AI system is playing
     protected Random ran;
+    private final int[] rowCheckValues = {1, 1, 0, -1, -1, -1, 0, 1};
+    private final int[] columnCheckValues = {0, -1, -1, -1, 0, 1, 1, 1};
+    private int tempRow = 0;
+    private int tempColumn = 0;
+    private char playerPiece = 'O';
+    private char opponentPiece = 'X';
+    int totalValue = 0;
     
     public OthelloAI() {
         game = null;
@@ -51,11 +58,62 @@ public class OthelloAI extends AbstractAI {
         // First get the list of possible moves
         int player = game.getPlayer(); // Which player are we?
         ArrayList<OthelloGame.Action> actions = game.getActions(player);
+        
+        /*
+            Best move is:
+            
+            best of all my options
+            
+            all of my options are the worst of all their options
+            
+            all of their options are the best of all my options
+            
+            
+        */
 
-        // Now just pick of them out at random
+        // Now just pick one of them out at random
         int choice = ran.nextInt(actions.size());
+        
+        for (OthelloGame.Action action : actions) {
+            System.out.println("Move value for " + action.toString() + ":" + getMoveValue(board, action.row, action.col, player));
+        }
         return actions.get(choice).toString();
     }	
+    
+    private int getMoveValue(char[][] board, int row, int column, int player) {
+        playerPiece = player == 0 ? 'X' : 'O';
+        opponentPiece = player == 0 ? 'O' : 'X';
+        totalValue = 0;
+        /*
+            Check east, northeast, north, northwest, west, southwest, south, southeast
+        */
+        tempRow = 0;
+        tempColumn = 0;
+        
+        for (int i = 0; i < 8; i++) {
+            tempRow = row + rowCheckValues[i];
+            tempColumn = column + columnCheckValues[i];
+            if (!isValidSpace(board, tempRow, tempColumn, opponentPiece)) {
+                    continue;
+            } 
+            while (isValidSpace(board, tempRow, tempColumn, opponentPiece)) {
+                totalValue++;
+                tempRow += rowCheckValues[i];
+                tempColumn += columnCheckValues[i];
+            }
+        }
+        return totalValue;
+    }
+    
+    private boolean isValidSpace(char[][] board, int row, int column, char opponentPiece) {
+        if ((row >= board.length) || (column >= board.length) || (row < 0) || (column < 0)) {
+            return false;
+        }
+        if (board[row][column] != opponentPiece) {
+            return false;
+        }
+        return true;
+    }
 
     /**
      * Inform AI who the winner is
@@ -73,7 +131,6 @@ public class OthelloAI extends AbstractAI {
      **/
     @Override
     public synchronized void end() {
-        // This AI probably wants to store (in a file) what
-        // it has learned from playing all the games so far...
+        // Probably don't need anything here at the moment since it isn't LEARNING anything
     }
 }
