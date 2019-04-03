c7ba2e9fe5c117c535cd177f189c549b8264bc75
Austin <amax1240@gmail.com>
2019-04-03 12:26:58 -0400

    Before working with Duncan

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -18,6 +18,8 @@ from keras.layers import Dense, Activation
 import numpy as np
 import json
 import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
 import sys
 
 # Make the test set vs the training set.
@@ -32,47 +34,31 @@ def read_data(file):
         print("File Not Found: {0}.".format(err))
 
 
-def print_image(img, threshold):
-    for row in img:
-        for pixel in row:
-            print('.' if pixel > threshold else 'X', end='')
-        print()  # Newline at end of the row
-
-
 def file_get():
-    test_files = []
-    basepath = '/Users/AustinS/PycharmProjects/CSC350--AI/res'
+    files = []
+    basepath = '/Users/AustinS/PycharmProjects/CSC350--AI/res/'
     with os.scandir(basepath) as entries:
         for entry in entries:
             if entry.is_file():
-                    test_files.append('/Users/AustinS/PycharmProjects/CSC350--AI/res/' + entry.name)
-    return test_files
+                    files.append(basepath + entry.name)
+    return files
 
 
-def num_get():
+def info_get():
     files = file_get()
     test_set = []
     train_set = []
-    i = 0
-    for file in files:
-        if file[-6] == "9":
-            train_set.append(file[-8])
-        else:
-            test_set.append(file[-8])
-    return train_set, test_set
-
-
-def data_get():
-    files = file_get()
     test_data = []
     train_data = []
-    i = 0
     for file in files:
         if file[-6] == "9":
+            test_set.append(file[-8])
             test_data.append(read_data(file))
         else:
+            train_set.append(file[-8])
             train_data.append(read_data(file))
-    return train_data, test_data
+    return train_data, test_data, train_set, test_set
+
 
 def main():
     file_list = file_get()
@@ -84,10 +70,25 @@ def main():
     return img  # edit the return to only giv ethe array
 
 
+
+
+"""x_train = np.random.randint(2, size=(10000,2))   # Random input data
+y_train = np.array([[x and y, x^y] for (x,y) in x_train]) # The results (Carry,Add)"""
+
+x_train, x_test, y_train, y_test = np.array(info_get())
+
+# x_train = np.random.randint(2, size=(10000,2))   # Random input data
+print(len(x_train))
+print(len(y_train))
+print(x_train[12])
+print(y_train[12])
+# plt.imshow(x_train[12], cmap=plt.cm.binary)
+# plt.show()
+"""
 model = Sequential()
-model.add(Dense(units=20, input_dim=2)) # First (hidden) layer
+model.add(Dense(units=1024, input_dim=2)) # First (hidden) layer
 model.add(Activation('sigmoid'))
-model.add(Dense(units=30)) # First (hidden) layer
+model.add(Dense(units=32)) # First (hidden) layer
 model.add(Activation('sigmoid'))
 model.add(Dense(units=2))   # Second, final (output) layer
 model.add(Activation('sigmoid'))
@@ -96,27 +97,12 @@ model.compile(loss='mean_squared_error',
               optimizer='sgd',
               metrics=['accuracy'])
 
-"""x_train = np.random.randint(2, size=(10000,2))   # Random input data
-y_train = np.array([[x and y, x^y] for (x,y) in x_train]) # The results (Carry,Add)"""
-
-main()
-x_train, x_test = data_get()
-y_train, y_test = num_get()
-
-print(x_train)
-
-"""
-
-x_train = np.array(main())
-# x_train = np.random.randint(2, size=(10000,2))   # Random input data
-y_train = np.array([[x and y, x^y] for (x,y) in x_train])
-
 
 # Train the model, iterating on the data in batches of 32 samples (try batch_size=1)
-model.fit(x_train, y_train, epochs=10, batch_size=1)
+model.fit(x_train, y_train, epochs=10, batch_size=2)
 
-x_test = np.random.randint(2, size=(1000, 2))   # Random input data
-y_test = np.array([[x and y, x ^ y] for (x, y) in x_test]) # The results (Carry,Add)
+# x_test = np.random.randint(2, size=(1000, 2))   # Random input data
+# y_test = np.array([[x and y, x ^ y] for (x, y) in x_test]) # The results (Carry,Add)
 
 # Evaluate the model from a sample test data set
 score = model.evaluate(x_test, y_test)
@@ -129,8 +115,13 @@ x_input = np.array([[0,0], [0,1], [1,0], [1,1]])
 y_output = model.predict(x_input)
 print("Result of {} is {}.".format(x_input, y_output))
 
+
+
+
+
+
 # this is the main line to run
 if __name__ == "__main__":
     main()
-    
-    """
+
+"""
